a4df72127c86b2e87ac5b36096bac84b8e80c793
Товкус Артем Евгеньевич <PIBO0121020@sibupk.su>
2024-05-11 15:42:08 +0700

    Добавьте файлы проекта.

---
+++ Reguliarka/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Reguliarka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"input.txt"); //чтение массива строк сделать для начал строки 
+            for (int i = 0; i < input.Length; ++i)
+            {
+                Regex regex = new Regex(@"a aaaa ");
+                
+            }
+        }
+
+    }
+
+}
