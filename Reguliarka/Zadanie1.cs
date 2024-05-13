473b730595d66f07bdde8b5a27d8ed472802dc9f
ARTEM K <tovkus.artem@mail.ru>
2024-05-14 00:09:54 +0700

    2 и 3 задание

--- Reguliarka/Zadanie1.cs
+++ Reguliarka/Zadanie1.cs
@@ -8,26 +8,27 @@ using System.Text.RegularExpressions;
 
 namespace Reguliarka
 {
-    class Program
+    class Zadanie1
     {
         static void Main(string[] args)
         {
             string[] input = File.ReadAllLines(@"input.txt"); //чтение массива строк сделать для начал строки 
             for (int i = 0; i < input.Length; ++i)
             {
-                Regex regex = new Regex(@"a aaaa ");
-
-                //следующие строки выведут «ОК»
-                if (r3.IsMatch(str))
+                Regex regex1 = new Regex(@"^a$|^a{6}$|^a\sa{2}\sa$");
+                Regex regex2 = new Regex(@"^\w{5,}$");
+                Regex regex3 = new Regex(@"^[\S\d]+@[\S\d]+\.\w{2,}$");
+                string line = input[i];
+                if (regex1.IsMatch(line) || regex2.IsMatch(line) || regex3.IsMatch(line))
                 {
                     Console.WriteLine("ОК!");
                 }
                 else
                 {
                     Console.WriteLine("Not ОК ☹");
-                }    
+                }
+            }
         }
 
     }
-
 }
